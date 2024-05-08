5a207e03bf628088d764488a845e469395d63b39
Uday Samsani <satyasaisamsani@gmail.com>
2024-05-08 14:48:35 -0400

    Modified api

--- api/cloudinary/[..media].ts
+++ api/cloudinary/[..media].ts
@@ -12,7 +12,7 @@ module.exports = createMediaHandler({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "",
   api_key: process.env.CLOUDINARY_API_KEY || "",
   api_secret: process.env.CLOUDINARY_API_SECRET || "",
-  authorized: async req => {
+  authorized: async (req: Request) => {
     try {
       if (process.env.NODE_ENV == "development") {
         return true;
@@ -20,7 +20,7 @@ module.exports = createMediaHandler({
 
       const user = await isAuthorized(req);
 
-      return !!(user && user.verified);
+      return (user && user.verified);
     } catch (e) {
       console.error(e);
       return false;
