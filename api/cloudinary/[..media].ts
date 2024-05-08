a40ccf1a309466cf9f38ae2ddebf8cdd46e38885
Uday Samsani <satyasaisamsani@gmail.com>
2024-05-08 16:11:28 -0400

    Modified for api

--- api/cloudinary/[..media].ts
+++ api/cloudinary/[..media].ts
@@ -7,7 +7,7 @@ const {
 
 export const config = mediaHandlerConfig;
 
-export default createMediaHandler({
+createMediaHandler({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "",
   api_key: process.env.CLOUDINARY_API_KEY || "",
   api_secret: process.env.CLOUDINARY_API_SECRET || "",
