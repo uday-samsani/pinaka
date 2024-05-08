53ed20e2b0bff1b666fcb56c37dc0d6a107d0fab
Uday Samsani <satyasaisamsani@gmail.com>
2024-05-08 16:20:22 -0400

    Modified for api

--- api/cloudinary/[..media].ts
+++ api/cloudinary/[..media].ts
@@ -5,9 +5,9 @@ const {
   mediaHandlerConfig,
 } = require("next-tinacms-cloudinary/dist/handlers");
 
-const config = mediaHandlerConfig;
+export const config = mediaHandlerConfig;
 
-createMediaHandler({
+export default createMediaHandler({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "",
   api_key: process.env.CLOUDINARY_API_KEY || "",
   api_secret: process.env.CLOUDINARY_API_SECRET || "",
