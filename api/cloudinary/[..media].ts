553982738cb1d62e4d5954da1ccf8289df3523c7
Uday Samsani <satyasaisamsani@gmail.com>
2024-05-08 16:49:35 -0400

    Modified for api

--- api/cloudinary/[..media].ts
+++ api/cloudinary/[..media].ts
@@ -3,6 +3,7 @@ const { isAuthorized } = require("@tinacms/auth");
 const {
   createMediaHandler,
   mediaHandlerConfig,
+  deleteMediaHandler,
 } = require("next-tinacms-cloudinary/dist/handlers");
 
 module.exports.config = mediaHandlerConfig;
@@ -19,6 +20,27 @@ module.exports = createMediaHandler({
 
       const user = await isAuthorized(req);
 
+      // return (user && user.verified);
+      return true;
+    } catch (e) {
+      console.error(e);
+      // return false;
+      return false;
+    }
+  },
+});
+
+module.exports = deleteMediaHandler({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "",
+  api_key: process.env.CLOUDINARY_API_KEY || "",
+  api_secret: process.env.CLOUDINARY_API_SECRET || "",
+  authorized: async (req: Request, res: Response) => {
+    try {
+      if (process.env.NODE_ENV === "development") {
+        return true;
+      }
+
+      const user = await isAuthorized(req);
       // return (user && user.verified);
       return true;
     } catch (e) {
