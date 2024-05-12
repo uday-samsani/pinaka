80dce71ea8a61a2a5a19ed77e10921441dcff16c
Uday Samsani <satyasaisamsani@gmail.com>
2024-05-11 21:55:12 -0400

    Modified layout button

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,7 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
-import { SITE } from "./src/config";
+import { SITE } from "@config";
 
 // https://astro.build/config
 export default defineConfig({
